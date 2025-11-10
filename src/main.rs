d34458078d7bab2fa726120e4a6d22771a36b895
MatthewScargill <matthewscargilluni@gmail.com>
2025-11-10 20:58:03 +0000

    saying hello back

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Hello, {}!", args.name);
+}
